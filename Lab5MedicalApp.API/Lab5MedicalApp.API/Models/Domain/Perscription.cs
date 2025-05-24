2029ad65a91bb22d0012f887bdc55fc2d3165726
Maciej Litwinowicz <s21535@pjwstk.edu.pl>
2025-05-24 15:18:15 +0200

    Kosmetyka modelu Perscriptions

--- Lab5MedicalApp.API/Lab5MedicalApp.API/Models/Domain/Perscription.cs
+++ Lab5MedicalApp.API/Lab5MedicalApp.API/Models/Domain/Perscription.cs
@@ -7,12 +7,13 @@ public class Perscription
 {
     [Key]
     public int IdPerscription { get; set; }
-    public DateOnly Date { get; set; }
-    public DateOnly DueDate { get; set; }
     [ForeignKey(nameof(Patient))]
     public int IdPatient { get; set; }
     [ForeignKey(nameof(Doctor))]
     public int IdDoctor { get; set; }
+    
+    public DateOnly Date { get; set; }
+    public DateOnly DueDate { get; set; }
     
     public ICollection<PerscriptionMedicament> PerscriptionMedicaments { get; set; }
     public Patient Patient { get; set; }
